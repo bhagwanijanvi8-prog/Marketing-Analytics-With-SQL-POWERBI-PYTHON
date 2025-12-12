375d5096e96565fa2480d3ed56c1e12146a0a712
janvi bhagwani <bhagwanijanvi8@gmail.com>
2025-12-13 02:37:36 +0530

    Create dim_customers.sql

---
+++ dim_customers.sql
@@ -0,0 +1,16 @@
+-- SQL Statement to join dim_customers with dim_geography to enrich customer data with geographic information
+
+SELECT 
+c.CustomerID,
+c.CustomerName,
+c.Email, 
+c.Gender,
+c.Age,
+g.Country,
+g.City
+FROM 
+   dbo.customers as c
+LEFT JOIN 
+   dbo.geography as g
+ON
+   c.GeographyID = g.GeographyID;
